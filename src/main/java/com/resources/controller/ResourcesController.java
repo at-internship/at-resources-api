580fd0e6db5b4da7759ed70e693fd62b52f8aa8d
Carla <Carla@LAPTOP-4GM5TKBV>
2020-10-29 18:17:13 -0600

    Log4j2 Implementation

--- src/main/java/com/resources/controller/ResourcesController.java
+++ src/main/java/com/resources/controller/ResourcesController.java
@@ -3,12 +3,19 @@ package com.resources.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 @RestController
+@Slf4j
 public class ResourcesController {
 
 	@GetMapping("get")
 	public String getting(){
+		log.info("info log");
+		log.warn("warn log ");
+		log.error("error log");
+		log.debug("debug log");
 	return ("Hello at-resources-api"); 
 	}
 }
