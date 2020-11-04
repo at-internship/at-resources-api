85a83a9d1457b0925d53693a67ff7702cac6622e
JoeDyenz <joedyenz@gmail.com>
2020-11-04 13:18:11 -0600

    US-RESOURCES-01 merging part 5

--- src/main/java/com/resources/repository/StoryRepository.java
+++ src/main/java/com/resources/repository/StoryRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.resources.domain.Story;
+import com.resources.model.Story;
 
 @Repository 
 public interface StoryRepository extends MongoRepository<Story,String>{
