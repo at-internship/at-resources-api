46cdc4b3120b0e9816dfd554a7be72541b616c3a
KevinRSantillan <63812607+KevinRSantillan@users.noreply.github.com>
2020-11-12 17:12:47 -0600

    feature/AT-RESOURCES-23
    String Format

--- src/main/java/com/resources/services/ResourcesServiceImpl.java
+++ src/main/java/com/resources/services/ResourcesServiceImpl.java
@@ -66,7 +66,7 @@ public class ResourcesServiceImpl implements ResourcesService {
 			storyRequestDTO.setUserId(story.getUser_id().toString());
 			return storyRequestDTO;
 		} else {
-      throw new NotFoundException(HttpExceptionMessage.IDNOTFOUND, PathErrorMessage.pathApiDelete,HttpStatus.NOT_FOUND);
+      throw new NotFoundException(String.format(HttpExceptionMessage.IDNOTFOUND,id),PathErrorMessage.pathApiDelete,HttpStatus.NOT_FOUND);
 		}
 	}//END updateStory
 	
@@ -75,7 +75,7 @@ public class ResourcesServiceImpl implements ResourcesService {
 		if (storyRepository.existsById(id)) {
 			storyRepository.deleteById(id);
 		} else {
-			throw new NotFoundException(HttpExceptionMessage.IDNOTFOUND, PathErrorMessage.pathApiDelete,HttpStatus.NOT_FOUND);
+      throw new NotFoundException(String.format(HttpExceptionMessage.IDNOTFOUND,id),PathErrorMessage.pathApiDelete,HttpStatus.NOT_FOUND);
 		}
 	}//End deleteStory
 
